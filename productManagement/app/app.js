04f3431122a99424e22dbbd89b9ea7539eb4fb7b
Tommy Crafton <twcrafton@gmail.com>
2016-04-25 02:25:36 -0500

    Updated

--- productManagement/app/app.js
+++ productManagement/app/app.js
@@ -3,7 +3,23 @@
     var app = angular.module("productManagement",
                             ["common.services",
                              "ui.router",
+                             "ui.mask",
+                             "ui.bootstrap",
+                             "angularCharts",
                              "productResourceMock"]);
+
+    app.config(function ($provide) {
+        $provide.decorator("$exceptionHandler",
+            ["$delegate",
+                function ($delegate) {
+                    return function (exception, cause) {
+                        exception.message = "Please contact the Help Desk! \n Message: " +
+                                                                exception.message;
+                        $delegate(exception, cause);
+                        alert(exception.message);
+                    };
+                }]);
+    });
 
     app.config(["$stateProvider",
                 "$urlRouterProvider",
@@ -22,9 +38,65 @@
                     controller: "ProductListCtrl as vm"
                 })
                 .state("productEdit", {
+                    abstract: true,
                     url: "/products/edit/:productId",
                     templateUrl: "app/products/productEditView.html",
-                    controller: "ProductEditCtrl as vm"
+                    controller: "ProductEditCtrl as vm",
+                    resolve: {
+                        productResource: "productResource",
+
+                        product: function (productResource, $stateParams) {
+                            var productId = $stateParams.productId;
+                            return productResource.get({ productId: productId }).$promise;
+                        }
+                    }
+                })
+                .state("productEdit.info", {
+                    url: "/info",
+                    templateUrl: "app/products/productEditInfoView.html"
+                })
+                .state("productEdit.price", {
+                    url: "/price",
+                    templateUrl: "app/products/productEditPriceView.html"
+                })
+                .state("productEdit.tags", {
+                    url: "/tags",
+                    templateUrl: "app/products/productEditTagsView.html"
+                })
+                .state("productDetail", {
+                    url: "/products/:productId",
+                    templateUrl: "app/products/productDetailView.html",
+                    controller: "ProductDetailCtrl as vm",
+                    resolve: {
+                        productResource: "productResource",
+
+                        product: function (productResource, $stateParams) {
+                            var productId = $stateParams.productId;
+                            return productResource.get({ productId: productId }).$promise;
+                        }
+                    }
+                })
+                .state("priceAnalytics", {
+                    url: "/priceAnalytics",
+                    templateUrl: "app/prices/priceAnalyticsView.html",
+                    controller: "PriceAnalyticsCtrl",
+                    resolve: {
+                        productResource: "productResource",
+
+                        products: function (productResource) {
+                            return productResource.query(function (response) {
+
+                            },
+                            function (response) {
+                                if (response.status == 404) {
+                                    alert("Error accessing resource: " +
+                                        response.config.method + " " + response.config.url);
+                                } else {
+                                    alert(response.statusText);
+                                }
+                            }).$promise;
+                        }
+                    }
                 })
         }]
     );
